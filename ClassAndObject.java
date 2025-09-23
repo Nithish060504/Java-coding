e204a20bb49a18e6afeaff9a35e8d126caebeae7
Nithish M <mnithish611@gmail.com>
2025-09-23 15:25:47 +0530

    Add files via upload

---
+++ ClassAndObject.java
@@ -0,0 +1,27 @@
+package com.p2;
+import java.io.*;
+import java.util.Scanner;
+class Student {
+	int id;
+	String name;
+	
+	void display(){
+		System.out.println("ID:" +id);
+		System.out.println("Name:" +name);
+	}
+	
+}
+public class ClassAndObject {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+        Student ram=new Student();
+        System.out.println("enter id:");
+        ram.id=sc.nextInt();
+        System.out.println("enter name:");
+        ram.name=sc.next();
+        
+        ram.display();
+	}
+
+}
---
+++ singleClass.java
@@ -0,0 +1,14 @@
+package com.p2;
+import java.io.*;
+public class singleClass {
+
+	 void display() {
+			System.out.println("Hai...");
+			
+		}
+		public static void main(String[] args) {
+			singleClass s=new singleClass();
+			s.display();
+	}
+
+}
